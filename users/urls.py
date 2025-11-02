82c9edd71184490a5f9bcb1eb83ff87b0c65a2f6
Tammy Healy <tamala556@gmail.com>
2025-11-02 15:44:02 +1100

    uodate urls.py with image information

--- users/urls.py
+++ users/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('', views.CustomUserList.as_view()),
     path('<int:pk>/', views.CustomUserDetail.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

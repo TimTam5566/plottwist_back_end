942c017b38a89e9aab695073da4073a923cd0caa
Tammy Healy <tamala556@gmail.com>
2025-12-14 15:10:10 +0800

    Register Pledge model in admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Pledge
 
 admin.site.register(Project)
-# Register your models here.
+admin.site.register(Pledge)
